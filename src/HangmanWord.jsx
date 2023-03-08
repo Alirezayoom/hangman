25480175748af4a639844712fc3e4c288aa36d99
alirezayoom <alirezayoom@gmail.com>
2023-03-09 00:16:51 +0430

    style added

--- src/HangmanWord.jsx
+++ src/HangmanWord.jsx
@@ -1,1 +1,30 @@
-export default function HangmanWord() {}
+export default function HangmanWord() {
+  const word = "hello";
+  const guessedLetters = ["h", "i", "l"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "1rem",
+        fontSize: "2.5rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: "5px solid black" }}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
