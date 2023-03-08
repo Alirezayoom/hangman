03ce37dee18d481b6a9dff5414d6b7adb4763ceb
alirezayoom <alirezayoom@gmail.com>
2023-03-08 23:17:14 +0430

    guessedLetters state added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
-  console.log(wordToGuess);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return <div>hi</div>;
 }
